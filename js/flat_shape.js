083c601b632fde949d62b2e21a85eaea7d657405
icedsquaredr <yeokh@i2r.a-star.edu.sg>
2016-02-03 10:51:45 +0800

    Update to public display of flat_shape.js.

--- js/flat_shape.js
+++ js/flat_shape.js
@@ -1,65 +1,89 @@
-// scene size
-var WIDTH = 400,
-    HEIGHT = 300;
+// These are the main components of a scene:
+var scene, renderer, camera;
 
-// camera attributes
-var VIEW_ANGLE = 45,
-ASPECT = WIDTH / HEIGHT,
-NEAR = 0.1,
-FAR = 10000;
+function define_sphere(radius, colour) {
+    // sphere example
+    var radius = radius,
+        segments = 16,
+        rings = 16;
 
-// WebGL: renderer, camera, scene
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera(
+    // set the geometry of the sphere
+    var sampleSphereGeometry =
+        new THREE.SphereGeometry(
+            radius,
+            segments,
+            rings);
+
+    // use a Lambert material
+    var sphereMaterial = new THREE.MeshLambertMaterial(colour);
+
+    // create a mesh with the sphere geometry and material
+    var sphere = new THREE.Mesh(sampleSphereGeometry, sphereMaterial);
+
+    return sphere;
+}
+
+function init() {
+    scene = new THREE.Scene();
+    // scene size
+    var WIDTH = 400,
+        HEIGHT = 300;
+
+    // define and start renderer!
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(WIDTH, HEIGHT);
+
+    // also set the background colour of renderer
+    renderer.setClearColor(0xCCCCFF, 1);
+
+    // attach to DOM element
+    var container = document.getElementById("3d");
+    container.appendChild(renderer.domElement);
+
+    // camera attributes
+    var VIEW_ANGLE = 45,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 10000;
+
+    // WebGL: renderer, camera, scene
+    camera = new THREE.PerspectiveCamera(
                     VIEW_ANGLE,
                     ASPECT,
                     NEAR,
                     FAR);
-var scene = new THREE.Scene();
 
-// add the camera to the scene
-scene.add(camera);
+    // pull back camera sufficiently far away
+    camera.position.set(0, 0, 300);
+    
+    // add the camera to the scene
+    scene.add(camera);
 
-// pull back camera sufficiently far away
-camera.position.z = 300;
+    // If camera settings change, remember to update
+    // camera.aspect = ...
+    // camera.updateProjectionMatrix()
 
-// start renderer!
-renderer.setSize(WIDTH, HEIGHT);
+    // define a new pointlight and add to scene
+    var pointLight = new THREE.PointLight(0xFFFFFF);
+    pointLight.position.set(10, 50, 250)
+    scene.add(pointLight);
 
-// attach to DOM element
-var container = document.getElementById("3d");
-container.appendChild(renderer.domElement);
+    // define and add sphere to scene
+    var sample_sphere = define_sphere(50, { color: 0xCC00BB });
+    scene.add(sample_sphere);
 
-// define sphereMaterial
-var sphereMaterial = new THREE.MeshLambertMaterial(
-                        {
-                            color: 0xCC0099
-                        });
+    // If a predefined mesh is to be loaded, use loader
+    // loader.load( mesh_path, function (geometry) { ... });
+}
 
-// sphere example
-var radius = 50,
-    segments = 16,
-    rings = 16;
+function animate() {
+    
+    // Note: check out http://paulirish.com/2011/requestanimationframe-for-smart-animating
+    requestAnimationFrame(animate);
 
-var sampleSphereGeometry =
-    new THREE.SphereGeometry(
-        radius,
-        segments,
-        rings);
+    // Do the rendering here!
+    renderer.render(scene, camera);
+}
 
-// mesh with the sphere geometry
-var sphere = new THREE.Mesh(sampleSphereGeometry, sphereMaterial);
-
-// define a new pointlight
-var pointLight = new THREE.PointLight(0xFFFFFF);
-pointLight.position.x = 10;
-pointLight.position.y = 50;
-pointLight.position.z = 100;
-
-// add sphere to scene
-scene.add(sphere);
-
-// add light to scene
-scene.add(pointLight);
-
-renderer.render(scene, camera);
+init();
+animate();
